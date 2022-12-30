3dc7bee8ae535ab575c8783575ca1e3b94bb60f8
Suraj Khokrale <88073175+SurajK2025@users.noreply.github.com>
2022-12-30 14:11:46 +0530

    Update server.js

--- ExpressCrudApp/server.js
+++ ExpressCrudApp/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const routes = require('./router');
 const app = express();
 routes(app);
-const port = 8080;
+const port = 8081;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
