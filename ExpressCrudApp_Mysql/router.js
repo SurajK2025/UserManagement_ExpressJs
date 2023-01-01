c5b07e011165cd4439534201e89ca6209f1326dc
SurajK2025 <suraj.khokrale@rediffmail.com>
2023-01-01 23:09:47 +0530

    New Project

--- ExpressCrudApp_Mysql/router.js
+++ ExpressCrudApp_Mysql/router.js
@@ -2,11 +2,7 @@ const userController = require("./controllers/usersController");
 
 const path = require("path");
 module.exports = (app) => {
-  app
-    .route("/")
-    .get((req, res) =>
-      res.sendFile(path.join(__dirname, "public/views/index.html"))
-    );
+  app.route("/").get((req, res) => res.sendFile(path.join(__dirname, "public/views/index.html")));
 
   app.route("/getUsers").get(userController.getAllUsers);
 
